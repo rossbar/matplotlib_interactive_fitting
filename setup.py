ce3cf24c8700c79a4e02bf849c223aa6f721ece2
Ross Barnowski <rossbar@berkeley.edu>
2019-01-29 15:36:19 -0800

    Added setup.py to improve portability.
    Working towards being able to install with pip -e

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='matplotlib_interactive_fitting',
+    version='0.1dev',
+    packages=['.',],
+    license='BSD3',
+    long_description=open('README.md').read(),
+)
