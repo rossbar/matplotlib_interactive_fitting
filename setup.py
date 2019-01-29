113744b58415f060d7413150d3b8f2fe7c897811
Ross Barnowski <rossbar@berkeley.edu>
2019-01-29 15:53:07 -0800

    Project reorganization: move src to package.
    Attempt to reorganize to better match the example project structures
    from python packaging tutorials.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='matplotlib_interactive_fitting',
     version='0.1dev',
-    packages=['.',],
+    packages=['mpl_fitting',],
     license='BSD3',
     long_description=open('README.md').read(),
 )
